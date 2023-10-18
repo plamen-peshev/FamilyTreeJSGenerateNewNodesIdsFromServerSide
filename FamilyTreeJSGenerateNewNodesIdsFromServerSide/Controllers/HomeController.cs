b023bced44a68f57193e0e8b1d5c3b16baf33869
plamen-peshev <plamen.peshev@live.com>
2023-10-18 23:08:07 +0300

    Add project files.

---
+++ FamilyTreeJSGenerateNewNodesIdsFromServerSide/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace FamilyTreeJSGenerateNewNodesIdsFromServerSide.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Update([FromBody] JsonElement args)
+        {
+            System.Threading.Thread.Sleep(1000);
+            var addNodesData = args.GetProperty("addNodesData").EnumerateArray();
+
+            Random rnd = new Random();
+
+            var oldId_newId = new Dictionary<string, string>();
+            foreach (var node in addNodesData)
+            {
+                int serverId = rnd.Next();//Generate id from the server it could be SQL server
+                var clientId = node.GetProperty("id").ToString();
+                oldId_newId.Add(clientId, serverId.ToString());
+            }
+
+            return Json(oldId_newId);
+        }
+    }
+}
